691b153e2d77700050d9bbb1645ee4cd9a16f2d4
Shivanshu <shv.shivanshu@gmail.com>
2023-04-17 09:41:08 +0530

    Stash update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,8 @@ const Header = () => {
         <Navbar/>
         <div className="header-content flex flex-c text-center text-white">
           <h2 className="header-title text-capitalize">Find the book of your choice</h2><br/>
-          <p className="header-text fs-18 f2-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi voluptas omnis enim, illo tempora laudantium dicta libero perspiciatis id veniam perferendis nesciunt quaerat facilis quam.</p>
+          <p className="header-text fs-18 f2-3">This is a perfect website for book enthusiasts to find and get informations about their favorite books.</p>
+          <p className="header-text fs-18 f2-3">There is a search panel to search with the name of the book.</p>
           <SearchForm/>
         </div>
       </header>
